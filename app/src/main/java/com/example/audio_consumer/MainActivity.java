02d58373e4dda51ceb68f8e388b5bdece8fc310e
Edward Lu <edwardzlu98@gmail.com>
2019-08-16 11:31:22 -0400

    Added vocal test frames

--- app/src/main/java/com/example/audio_consumer/MainActivity.java
+++ app/src/main/java/com/example/audio_consumer/MainActivity.java
@@ -9,22 +9,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    StreamFrameBundler streamFrameBundler_;
-    StreamPlayer player_;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        streamFrameBundler_ = new StreamFrameBundler(TestFrames.AAC);
-        byte[][] bundles = streamFrameBundler_.getBundles();
-
-        player_ = new StreamPlayer();
-        player_.start();
-        for (int i = 0; i < bundles.length; i++) {
-            player_.giveAudioBundle(bundles[i]);
-        }
 
 
 

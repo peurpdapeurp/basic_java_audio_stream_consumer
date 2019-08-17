c4945d70229e7fa27183490cdfd7c48266df9788
Edward Lu <edwardzlu98@gmail.com>
2019-08-17 12:27:47 -0400

    Succeeded in getting ExoPlayer to play a .aac file from app's cache

--- app/src/main/java/com/example/audio_consumer/MainActivity.java
+++ app/src/main/java/com/example/audio_consumer/MainActivity.java
@@ -3,7 +3,20 @@ package com.example.audio_consumer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +27,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String filePath = getExternalCacheDir().getAbsolutePath() + "/" + "test.aac";
 
+        AudioProcessingHelpers.writeTestAudioToFile(filePath);
 
+        ExoPlayer player = ExoPlayerFactory.newSimpleInstance(this);
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "test"));
+        // This is the MediaSource representing the media to be played.
+        MediaSource audioSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.fromFile(new File(filePath)));
+
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                String playbackStateString = "";
+                switch (playbackState) {
+                    case Player.STATE_IDLE:
+                        playbackStateString = "STATE_IDLE";
+                        break;
+                    case Player.STATE_BUFFERING:
+                        playbackStateString = "STATE_BUFFERING";
+                        break;
+                    case Player.STATE_READY:
+                        playbackStateString = "STATE_READY";
+                        break;
+                    case Player.STATE_ENDED:
+                        playbackStateString = "STATE_ENDED";
+                        break;
+                    default:
+                        playbackStateString = "UNEXPECTED STATE CODE (" + playbackState + ")";
+                }
+                Log.d(TAG, "Exoplayer state changed to: " + playbackStateString);
+            }
+        });
+
+        player.prepare(audioSource);
+
+        player.setPlayWhenReady(true);
     }
 
 }

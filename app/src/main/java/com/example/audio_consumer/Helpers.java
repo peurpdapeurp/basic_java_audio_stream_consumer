f7b0312d7d85a37ab3c8580a8da7c24ee75d007f
Edward Lu <edwardzlu98@gmail.com>
2019-08-17 11:09:06 -0400

    Cleaned up project, prepared to implement Exoplayer based solution

--- app/src/main/java/com/example/audio_consumer/Helpers.java
+++ app/src/main/java/com/example/audio_consumer/Helpers.java
@@ -12,8 +12,6 @@ import java.util.Arrays;
 
 public class Helpers {
 
-    public static final byte[] temp_key = new byte[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
     public static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];

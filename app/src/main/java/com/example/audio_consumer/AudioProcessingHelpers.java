a88ba74d846981af1323ce948785050265712865
Edward Lu <edwardzlu98@gmail.com>
2019-08-20 11:53:47 -0400

    Added the exoplayer2 source code into my project in order to add debug statements and figure out why the exoplayer is not leaving STATE_BUFFERING when passed ADTS frames from my InputStreamDataSource

--- app/src/main/java/com/example/audio_consumer/AudioProcessingHelpers.java
+++ app/src/main/java/com/example/audio_consumer/AudioProcessingHelpers.java
@@ -1,13 +1,27 @@
 package com.example.audio_consumer;
 
+import android.content.Context;
 import android.media.AudioFormat;
 import android.media.MediaFormat;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.util.Log;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 
@@ -130,6 +144,112 @@ public class AudioProcessingHelpers {
             e.printStackTrace();
         }
         return musicSampleArray;
+    }
+
+    public static void playAudioFileFromCacheWithExoPlayer(Context ctx, String filePath) {
+
+        ExoPlayer player = ExoPlayerFactory.newSimpleInstance(ctx);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(ctx,
+                Util.getUserAgent(ctx, "test"));
+        MediaSource audioSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.fromFile(new File(filePath)));
+
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                String playbackStateString = "";
+                switch (playbackState) {
+                    case Player.STATE_IDLE:
+                        playbackStateString = "STATE_IDLE";
+                        break;
+                    case Player.STATE_BUFFERING:
+                        playbackStateString = "STATE_BUFFERING";
+                        break;
+                    case Player.STATE_READY:
+                        playbackStateString = "STATE_READY";
+                        break;
+                    case Player.STATE_ENDED:
+                        playbackStateString = "STATE_ENDED";
+                        break;
+                    default:
+                        playbackStateString = "UNEXPECTED STATE CODE (" + playbackState + ")";
+                }
+                Log.d(TAG, "(Playing from cache) Exoplayer state changed to: " + playbackStateString);
+            }
+        });
+
+        player.prepare(audioSource);
+
+        player.setPlayWhenReady(true);
+
+    }
+
+    public static void playAudioFileFromInputStreamWithExoPlayer(Context ctx, byte[][] frames) {
+
+        InputStream is_;
+        OutputStream os_;
+
+        ParcelFileDescriptor[] parcelFileDescriptors_;
+        ParcelFileDescriptor parcelRead_;
+        ParcelFileDescriptor parcelWrite_;
+
+        try {
+            parcelFileDescriptors_ = ParcelFileDescriptor.createPipe();
+            parcelRead_ = new ParcelFileDescriptor(parcelFileDescriptors_[0]);
+            parcelWrite_ = new ParcelFileDescriptor(parcelFileDescriptors_[1]);
+
+            is_ = new ParcelFileDescriptor.AutoCloseInputStream(parcelRead_);
+            os_ = new ParcelFileDescriptor.AutoCloseOutputStream(parcelWrite_);
+
+            os_.write(frames[0]);
+            os_.write(frames[1]);
+            os_.write(frames[2]);
+            os_.write(frames[3]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        ExoPlayer player = ExoPlayerFactory.newSimpleInstance(ctx);
+
+        // This is the MediaSource representing the media to be played.
+        MediaSource audioSource = new ProgressiveMediaSource.Factory(
+                () -> {
+                    InputStreamDataSource dataSource =
+                            new InputStreamDataSource(is_);
+                    return dataSource;
+                })
+                .createMediaSource(Uri.parse("fake_uri"));
+
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                String playbackStateString = "";
+                switch (playbackState) {
+                    case Player.STATE_IDLE:
+                        playbackStateString = "STATE_IDLE";
+                        break;
+                    case Player.STATE_BUFFERING:
+                        playbackStateString = "STATE_BUFFERING";
+                        break;
+                    case Player.STATE_READY:
+                        playbackStateString = "STATE_READY";
+                        break;
+                    case Player.STATE_ENDED:
+                        playbackStateString = "STATE_ENDED";
+                        break;
+                    default:
+                        playbackStateString = "UNEXPECTED STATE CODE (" + playbackState + ")";
+                }
+                Log.d(TAG, "(Playing from input stream) Exoplayer state changed to: " + playbackStateString);
+            }
+        });
+
+        player.prepare(audioSource);
+
+        player.setPlayWhenReady(true);
     }
 
 }
--- app/src/main/java/com/example/audio_consumer/MainActivity.java
+++ app/src/main/java/com/example/audio_consumer/MainActivity.java
@@ -3,36 +3,11 @@ package com.example.audio_consumer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.util.Log;
-
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    InputStream is_;
-    OutputStream os_;
-
-    ParcelFileDescriptor[] parcelFileDescriptors_;
-    ParcelFileDescriptor parcelRead_;
-    ParcelFileDescriptor parcelWrite_;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,61 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
         AudioProcessingHelpers.writeTestAudioToFile(filePath);
 
-        try {
-            parcelFileDescriptors_ = ParcelFileDescriptor.createPipe();
-            parcelRead_ = new ParcelFileDescriptor(parcelFileDescriptors_[0]);
-            parcelWrite_ = new ParcelFileDescriptor(parcelFileDescriptors_[1]);
+        AudioProcessingHelpers.playAudioFileFromCacheWithExoPlayer(this, filePath);
 
-            is_ = new ParcelFileDescriptor.AutoCloseInputStream(parcelRead_);
-            os_ = new ParcelFileDescriptor.AutoCloseOutputStream(parcelWrite_);
+        AudioProcessingHelpers.playAudioFileFromInputStreamWithExoPlayer(this, TestFrames.MUSIC_ADTS_FRAME_BUFFERS);
 
-            os_.write(TestFrames.MUSIC_ADTS_FRAME_BUFFERS[0]);
-            os_.write(TestFrames.MUSIC_ADTS_FRAME_BUFFERS[1]);
-            os_.write(TestFrames.MUSIC_ADTS_FRAME_BUFFERS[2]);
-            os_.write(TestFrames.MUSIC_ADTS_FRAME_BUFFERS[3]);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        ExoPlayer player = ExoPlayerFactory.newSimpleInstance(this);
-
-        // This is the MediaSource representing the media to be played.
-        MediaSource audioSource = new ProgressiveMediaSource.Factory(
-                () -> {
-                    InputStreamDataSource dataSource =
-                            new InputStreamDataSource(is_);
-                    return dataSource;
-                })
-                .createMediaSource(Uri.parse("fake_uri"));
-
-        player.addListener(new Player.EventListener() {
-            @Override
-            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-                String playbackStateString = "";
-                switch (playbackState) {
-                    case Player.STATE_IDLE:
-                        playbackStateString = "STATE_IDLE";
-                        break;
-                    case Player.STATE_BUFFERING:
-                        playbackStateString = "STATE_BUFFERING";
-                        break;
-                    case Player.STATE_READY:
-                        playbackStateString = "STATE_READY";
-                        break;
-                    case Player.STATE_ENDED:
-                        playbackStateString = "STATE_ENDED";
-                        break;
-                    default:
-                        playbackStateString = "UNEXPECTED STATE CODE (" + playbackState + ")";
-                }
-                Log.d(TAG, "Exoplayer state changed to: " + playbackStateString);
-            }
-        });
-
-        player.prepare(audioSource);
-
-        player.setPlayWhenReady(true);
     }
 
 }
